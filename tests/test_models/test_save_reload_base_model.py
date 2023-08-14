05f6668a66d6941dac70fb94bb690a9e45dce2cb
jayricka <jayricka17@gmail.com>
2023-08-14 05:22:16 +0300

    console

--- tests/test_models/test_save_reload_base_model.py
+++ tests/test_models/test_save_reload_base_model.py
@@ -1,27 +1,37 @@
-import os
-from models import storage
+#!/usr/bin/python3
+import unittest
 from models.base_model import BaseModel
+from models import storage
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+class TestSaveReloadBaseModel(unittest.TestCase):
+    """Test cases for saving and reloading BaseModel."""
+    
+    def test_save_reload(self):
+        """Test saving and reloading a BaseModel object."""
+        # Create a new BaseModel object
+        new_base_model = BaseModel()
+        new_base_model.name = "My_First_Model"
+        new_base_model.my_number = 89
+        new_base_model.save()
+        
+        # Reload objects from storage (simulating a new session)
+        storage.reload()
+        
+        # Verify the reloaded object
+        # Construct the correct key for accessing the reloaded object
+        key = f"BaseModel.{new_base_model.id}"
+        reloaded_base_model = storage.all().get(key)
+        
+        # Check if the reloaded object is found
+        self.assertIsNotNone(reloaded_base_model)
+        
+        # Compare attributes of the original and reloaded objects
+        self.assertEqual(new_base_model.id, reloaded_base_model.id)
+        self.assertEqual(new_base_model.created_at, reloaded_base_model.created_at)
+        self.assertEqual(new_base_model.updated_at, reloaded_base_model.updated_at)
+        self.assertEqual(new_base_model.name, reloaded_base_model.name)
+        self.assertEqual(new_base_model.my_number, reloaded_base_model.my_number)
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()  # Save the new object
-print(my_model)
-
-# Ensure the object is saved to file.json
-storage.save()
-
-# Construct the absolute path to file.json using os.path.join
-file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../file.json")
-
-# Print the contents of file.json
-with open(file_path, "r") as json_file:
-    print(json_file.read())
+if __name__ == '__main__':
+    unittest.main()
 
