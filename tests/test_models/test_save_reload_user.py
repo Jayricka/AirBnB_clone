8497c4262c08e2c58a2fd6e58ede639f02cdc667
jayricka <jayricka17@gmail.com>
2023-08-21 01:53:41 +0300

    console updt

--- tests/test_models/test_save_reload_user.py
+++ tests/test_models/test_save_reload_user.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-import sys
-import os
-
-# Get the current directory of the script
-current_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Add the project root directory to sys.path
-project_root = os.path.abspath(os.path.join(current_dir, "../../"))
-sys.path.append(project_root)
-
 from models import storage
+from models.base_model import BaseModel
 from models.user import User
 
-# Retrieve all objects from storage
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
@@ -26,7 +16,7 @@ my_user.last_name = "Bar"
 my_user.email = "airbnb@mail.com"
 my_user.password = "root"
 my_user.save()
-print("[User] ({}) {}".format(my_user.id, my_user.to_dict()))
+print(my_user)
 
 print("-- Create a new User 2 --")
 my_user2 = User()
@@ -34,4 +24,4 @@ my_user2.first_name = "John"
 my_user2.email = "airbnb2@mail.com"
 my_user2.password = "root"
 my_user2.save()
-print("[User] ({}) {}".format(my_user2.id, my_user2.to_dict()))
+print(my_user2)
