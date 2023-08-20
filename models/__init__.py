d9854eb45a5fccc9df8ce7d2b9f7f3278b42ba05
jayricka <jayricka17@gmail.com>
2023-08-20 13:44:16 +0300

    debugging

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@
 """This module initializes the FileStorage instance."""
 from models.engine.file_storage import FileStorage
 
-
+# Create a global FileStorage instance
 storage = FileStorage()
+
+# Load objects from the JSON file, if it exists
 storage.reload()
