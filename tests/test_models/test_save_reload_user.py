8429162425676f391b13ec7f1018ef6888931fa9
jayricka <jayricka17@gmail.com>
2023-08-20 13:14:52 +0300

    console

--- tests/test_models/test_save_reload_user.py
+++ tests/test_models/test_save_reload_user.py
@@ -21,7 +21,7 @@ print(my_user)
 print("-- Create a new User 2 --")
 my_user2 = User()
 my_user2.first_name = "John"
-my_user2.email = "airbnb2@mail.com"
+my_user2.email = "airbnb_2@mail.com"
 my_user2.password = "root"
 my_user2.save()
 print(my_user2)
